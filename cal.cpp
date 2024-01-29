e45f17bed700280702c23502f84d97ca143fdc9d
Aditya Raj <adistrim.dev@gmail.com>
2024-01-29 14:29:24 +0530

    added cal.cpp

---
+++ cal.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int a = 1;
+    int b = 2;
+
+    cout << add(a, b) << endl;
+
+    return 0;
+}
