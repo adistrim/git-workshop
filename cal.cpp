e5fb48b38b14be69d9f40337354be574165d40ae
Aditya Raj <adistrim.dev@gmail.com>
2024-01-29 14:47:26 +0530

    added .gitignore

--- cal.cpp
+++ cal.cpp
@@ -14,11 +14,17 @@ int multi(int a, int b) {
     return a * b;
 }
 
+int division(int a, int b) {
+    return a / b;
+}
+
 int main() {
     int a = 1;
     int b = 2;
 
     cout << add(a, b) << endl;
+
+    cout << "Why? " << endl;
 
     return 0;
 }
